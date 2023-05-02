5f61a21b340dcb6fba88efb8ed877d715daaf57a
Renangoes03 <renangoessantos9@gmail.com>
2023-05-02 08:58:27 -0300

    Api Chamada

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,5 +1,7 @@
+
 import { Component } from '@angular/core';
 import { Pokemon } from './pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -9,5 +11,16 @@ import { Pokemon } from './pokemon';
 export class PokemonComponent {
  pokemon: Pokemon = {} as Pokemon;
 
+ constructor(private pokemonService: PokemonService){ }
 
+ ngOnInit(): void { //inicialização do componente
+  this.loadPokemon(1);
+}
+ loadPokemon(num: number) {
+  this.pokemonService.getPokemon(num).subscribe({
+      next: pokemon => this.pokemon = pokemon
+    }
+  );
+
+}
 }
